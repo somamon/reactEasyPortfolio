e6d73b066df6084edf43fb04c3580223138be99d
somamon <geysers-canonic-0w@icloud.com>
2024-02-16 13:13:46 +0900

    ホーム画面でFirebaseから概念を読み込んで表示

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { doc, getDocs, collection, onSnapshot } from 'firebase/firestore';
+import { db } from '../firebase'; 
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [concepts, setConcepts] = useState([]);
 
-export default Home
+  useEffect(() => {
+    const getConcepts = async () => {
+      const conceptsCollection = collection(db, 'concepts'); // 'concepts'はFirestoreのコレクション名
+
+      try {
+        const querySnapshot = await getDocs(conceptsCollection);
+        const fetchedConcepts = [];
+        querySnapshot.forEach((doc) => {
+          fetchedConcepts.push({ id: doc.id, ...doc.data() });
+        });
+        setConcepts(fetchedConcepts);
+      } catch (error) {
+        console.error('概念の取得に失敗しました:', error);
+      }
+    };
+
+    // 初回読み込みと、データが更新されたときに再度読み込む
+    const unsubscribe = onSnapshot(collection(db, 'concepts'), () => {
+      getConcepts();
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <div>
+      <h2>ホーム</h2>
+      <ul>
+        {concepts.map((concept) => (
+          <li key={concept.id}>{concept.conceptValue}{concept.author.username}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
