a913673de4d8bfd3bb48b49e9d9c417de9a35e54
somamon <geysers-canonic-0w@icloud.com>
2024-02-16 13:14:02 +0900

    ヘッダーがログアウト時切り替わらないバグ修正

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Header = (isAuth) => {
+const Header = ({ isAuth }) => {
   return (
     <>
-    <Link to="/">ホーム</Link>
-    <Link to="/createconcept">新概念作成</Link>
-    {!isAuth ? <Link to="/login">ログイン</Link> :     <Link to="/logout">ログアウト</Link>}
-
+      <Link to="/">ホーム</Link>
+      {!isAuth ? (
+        <Link to="/login">ログイン</Link>
+      ) : (
+        <>
+          <Link to="/createconcept">新概念作成</Link>
+          <Link to="/logout">ログアウト</Link>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
